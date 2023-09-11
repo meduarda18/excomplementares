760163ef56cd516df52f65bfa6ce1e297a0a1255
Eduarda Souto <mariaeduardac604@gmail.com>
2023-09-11 16:25:41 -0300

    Iniciei estudante e estou testante pessoa

--- src/test/java/Pessoa.java
+++ src/test/java/Pessoa.java
@@ -1,39 +1,39 @@
-public class Pessoa{
+public class Pessoa {
   public String nome;
   public int idade;
   public String endereco;
 
-  public Pessoa(String nome, int idade, String endereco){
+  public Pessoa(String nome, int idade, String endereco) {
     this.nome = nome;
     this.idade = idade;
     this.endereco = endereco;
   }
 
-  public String getNome(){
+  public String getNome() {
     return nome;
   }
 
-  public void setNome(String nome){
+  public void setNome(String nome) {
     this.nome = nome;
   }
 
-  public int getIdade(){
+  public int getIdade() {
     return idade;
   }
 
-  public void setIdade(int idade){
+  public void setIdade(int idade) {
     this.idade = idade;
   }
 
-  public String getEndereco(){
+  public String getEndereco() {
     return endereco;
   }
 
-  public void setEndereco(String endereco){
+  public void setEndereco(String endereco) {
     this.endereco = endereco;
   }
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     Pessoa pessoa1 = new Pessoa("Maria", 18, "Rua João");
     System.out.println(pessoa1);
   }
